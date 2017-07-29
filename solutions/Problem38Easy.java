dda5a24a90c11736762fff6e0efbd1d305ecc56c
Logan Rouleau <loganrouleau@users.noreply.github.com>
2017-07-28 19:03:54 -0700

    Added solution

---
+++ solutions/Problem38Easy.java
@@ -0,0 +1,26 @@
+public class Problem38Easy{
+
+    public static String countAndSay(int n){
+        if(n <= 1){
+            return "1";
+        }
+        String input = countAndSay(n - 1);
+        int count = 0;
+        String output = "";
+        char currChar = input.charAt(0);
+        for(char c : input.toCharArray()){
+            if(c != currChar){
+                output = output + Integer.toString(count) + Character.toString(currChar);
+                currChar = c;
+                count = 0;
+            }
+            count++;
+        }
+        output = output + Integer.toString(count) + Character.toString(currChar);
+        return output;
+    }
+
+    public static void main(String[] args){
+        System.out.println(countAndSay(15));
+    }
+}
